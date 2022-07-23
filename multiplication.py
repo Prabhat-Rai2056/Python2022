fb961b2afd43ee0bcbcaa457cd7f551155459748
Prabhat <pravhatrai22@gmail.com>
2022-07-23 12:31:21 +0545

    prime number in a range

--- multiplication.py
+++
@@ -1,7 +0,0 @@
-p = input("enter the given number:")
-n = int(p)
-i = 1
-for i in range(10):
-    i = i+1
-    i = i*n
-    print(i)
--- prime.py
+++ prime.py
@@ -1,11 +1,11 @@
-start=input("start:")
-s=int(start)
-end=input("End:")
-e=int(end)
-for i in range(s,e+1):
-    if i>1:
-        for j in range(2,i):
-            if i%j==0:
+start = int(input("enter first number:"))
+end = int(input("enter second number:"))
+print("Prime numbers between", start, "and", end, "are:")
+
+for num in range(start, end + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
                 break
         else:
-            print(i)
+            print(num)
